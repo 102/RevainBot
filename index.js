d27b8deca7dbfb718d867a00d0c8f88ab5338e30
Roman Gusev <rgusev@hwdtech.ru>
2018-02-12 12:59:00 +0600

    Implement /price message handler

---
+++ index.js
@@ -0,0 +1,41 @@
+const TelegramBot = require("node-telegram-bot-api");
+const Cache = require("node-cache");
+const fetch = require("node-fetch");
+
+const CMC_API_URL = "https://api.coinmarketcap.com/v1/ticker/revain/";
+
+const cache = new Cache({ stdTTL: 60 });
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+const formatDate = new Intl.DateTimeFormat("en-US", {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  timeZoneName: "short"
+}).format;
+
+const getPrice = async () => {
+  let price = cache.get("price");
+
+  if (!price) {
+    const [{ price_usd }] = await (await fetch(CMC_API_URL)).json();
+
+    price = { price: price_usd, date: formatDate(new Date()) };
+    cache.set("price", price);
+  }
+
+  return price;
+};
+
+const formatPrice = ({ price, date }) => `R token price is $${price} (${date})`;
+
+bot.onText(/\/price/, async msg => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, formatPrice(await getPrice()));
+});
